e4bf596623f90a7874d5ee91a00ebc9d019e68ea
Stephen Margheim <stephen.margheim@gmail.com>
2023-12-10 16:50:13 +0100

    [GENERATED] bin/rails generate scaffold Comment post:belongs_to user:belongs_to body:text
          invoke  active_record
          create    db/migrate/20231210155000_create_comments.rb
          create    app/models/comment.rb
          invoke    test_unit
          create      test/models/comment_test.rb
          create      test/fixtures/comments.yml
          invoke  resource_route
           route    resources :comments
          invoke  scaffold_controller
          create    app/controllers/comments_controller.rb
          invoke    erb
          create      app/views/comments
          create      app/views/comments/index.html.erb
          create      app/views/comments/edit.html.erb
          create      app/views/comments/show.html.erb
          create      app/views/comments/new.html.erb
          create      app/views/comments/_form.html.erb
          create      app/views/comments/_comment.html.erb
          invoke    resource_route
          invoke    test_unit
          create      test/controllers/comments_controller_test.rb
          create      test/system/comments_test.rb
          invoke    helper
          create      app/helpers/comments_helper.rb
          invoke      test_unit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:new, :create]
