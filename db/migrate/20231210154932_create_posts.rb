6e9e566114ff088e28707ba1f39a4adfb9ffed67
Stephen Margheim <stephen.margheim@gmail.com>
2023-12-10 16:55:02 +0100

    Update generated migration

--- db/migrate/20231210154932_create_posts.rb
+++ db/migrate/20231210154932_create_posts.rb
@@ -2,10 +2,10 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.belongs_to :user, null: false, foreign_key: true
-      t.string :title
+      t.string :title, null: false
       t.text :description
       t.datetime :published_at
-      t.integer :comments_count
+      t.integer :comments_count, default: 0
 
       t.timestamps
     end
