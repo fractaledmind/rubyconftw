564d5e374255f93d57a3344f9292f95f4ff824f5
Stephen Margheim <stephen.margheim@gmail.com>
2023-12-16 13:49:48 +0800

    Setup branch-specific databases for local development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.after_initialize do
+    ActiveRecord::Tasks::DatabaseTasks.prepare_all
+  end
 end
