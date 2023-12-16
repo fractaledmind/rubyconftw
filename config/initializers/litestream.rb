fc9a514cd2681d23ef47fe6b8086d7bf3f4a8cd6
Stephen Margheim <stephen.margheim@gmail.com>
2023-12-16 13:48:44 +0800

    Setup Litestream for local development environment

--- config/initializers/litestream.rb
+++ config/initializers/litestream.rb
@@ -1,8 +1,8 @@
-litestream_credentials = Rails.application.credentials.litestream
+database_path = ActiveRecord::Base.connection_db_config.database
 
 Litestream.configure do |config|
-  config.database_path = ActiveRecord::Base.connection_db_config.database
-  config.replica_url = litestream_credentials.replica_url
-  config.replica_key_id = litestream_credentials.replica_key_id
-  config.replica_access_key = litestream_credentials.replica_access_key
+  config.database_path = database_path
+  config.replica_url = "s3://devbkt.localhost:9000/#{File.basename(database_path)}"
+  config.replica_key_id = "minioadmin"
+  config.replica_access_key = "minioadmin"
 end
